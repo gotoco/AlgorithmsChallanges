dff543137c443063ae335f275c369b8444e6b08f
skirki <Maciej.grochowski89@gmail.com>
2014-02-25 01:24:29 +0100

    #: Strongly connected component

---
+++ ruby/src/graph/strongly_connected_component.rb
@@ -0,0 +1,20 @@
+require "graph"
+
+class SccsGraph < Graph
+
+#In the mathematical theory of directed graphs, a graph is said to be strongly connected
+# if every vertex is reachable from every other vertex.
+# The strongly connected components of an arbitrary directed graph form a partition
+# into sub-graphs that are themselves strongly connected.
+# It is possible to test the strong connectivity of a graph,
+# or to find its strongly connected components, in linear time.
+#
+# * *Args*    :
+#   - +graph+ ->  source graph for with we run sccs
+# * *Return*    :   copy of graph object with sccs data
+#
+def sccs_algorithm()
+
+end
+
+end
